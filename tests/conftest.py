4cc6380a63a426dbde3714924997a4c2784bcdcb
Ognyan Moore <ognyan.moore@gmail.com>
2022-10-03 13:07:02 -0700

    Do registration testing on point clouds with alterations
    Add tests where a clipped copy of the foundation file is created, and
    the copy is manipulated via translation and rotation in a number of ways
    and each manipulation is tested against pytest.mark.parametrize

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,69 +1,20 @@
 import json
 import os
+import pathlib
 
 import pdal
 import pytest
 
 
-@pytest.fixture(scope="session")
-def tif_fnd() -> str:
-    foundation_file = os.path.abspath("tests/data/0_smallfnd.tif")
-    assert os.path.exists(foundation_file)
-    return foundation_file
-
-
-@pytest.fixture(scope="session")
-def tif_aoi() -> str:
-    aoi_file = os.path.abspath("tests/data/1_smallAOI.tif")
-    assert os.path.exists(aoi_file)
-    return aoi_file
-
-
-@pytest.fixture(scope="session")
-def dem_foundation() -> str:
-    foundation = os.path.abspath("tests/data/dem.tif")
-    return foundation
-
-
-@pytest.fixture(scope="session")
-def pc_foundation() -> str:
-    foundation = os.path.abspath("tests/data/pc.laz")
-    return foundation
+# @pytest.fixture(scope="session")
+# def tif_fnd() -> str:
+#     foundation_file = os.path.abspath("tests/data/0_smallfnd.tif")
+#     assert os.path.exists(foundation_file)
+#     return foundation_file
 
 
 # @pytest.fixture(scope="session")
-# def dem_aoi(dem_foundation) -> str:
-#     from osgeo import gdal
-#     dataset = gdal.Open(dem_foundation)
-
-
-@pytest.fixture(scope="session")
-def pc_aoi(pc_foundation: str) -> str:
-
-    output_file = os.path.abspath("tests/data/pc_aoi.laz")
-
-    reader = pdal.Reader(type="readers.las", filename=pc_foundation)
-    pipeline = reader.pipeline()
-    info = pipeline.quickinfo
-    bounds = info["readers.las"]["bounds"]
-
-    ratio = 0.25  # closer to 0.5, smaller the AOI
-    x_range = bounds["maxx"] - bounds["minx"]
-    xmin = bounds["minx"] + ratio * x_range
-    xmax = bounds["maxx"] - ratio * x_range
-
-    y_range = bounds["maxy"] - bounds["miny"]
-    ymin = bounds["miny"] + ratio * y_range
-    ymax = bounds["maxy"] - ratio * y_range
-
-    pipeline = [
-        pc_foundation,
-        {
-            "type": "filters.crop",
-            "bounds": f"([{xmin}, {xmax}], [{ymin}, {ymax}])",
-        },
-        {"type": "writers.las", "filename": output_file, "forward": "all"},
-    ]
-    p = pdal.Pipeline(json.dumps(pipeline))
-    p.execute()
-    return output_file
+# def tif_aoi() -> str:
+#     aoi_file = os.path.abspath("tests/data/1_smallAOI.tif")
+#     assert os.path.exists(aoi_file)
+#     return aoi_file
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,17 +1,72 @@
 import dataclasses
+import math
 import os
-from typing import TYPE_CHECKING
+import pathlib
+import tempfile
 
 import codem
 import pytest
+from point_cloud import manipulate_pc
+from point_cloud import pc_aoi
+from point_cloud import Rotation
+from point_cloud import Translation
 
 
-def test_basic_tif_registration(tif_fnd: str, tif_aoi: str) -> None:
-    codem_run_config = codem.CodemRunConfig(tif_fnd, tif_aoi)
+aoi_shapefile = os.path.abspath("tests/data/aoi_shapefile/aoi.shp")
+dem_foundation = os.path.abspath("tests/data/dem.tif")
+pc_foundation = os.path.abspath("tests/data/pc.laz")
+
+temporary_directory = os.path.abspath("tests/data/temporary")
+# temporary_directory = tempfile.TemporaryDirectory(dir=temporary_parent).name
+
+
+def make_aoi(aoi_temp_directory: str = temporary_directory) -> str:
+    aoi_file = pc_aoi(aoi_temp_directory, pc_foundation, aoi_shapefile)
+    return aoi_file
+
+
+aoi_file = make_aoi()
+
+
+@pytest.mark.parametrize(
+    "foundation,aoi",
+    [
+        pytest.param(pc_foundation, aoi_file, id="AOI Already Registered"),
+        pytest.param(
+            pc_foundation,
+            manipulate_pc(aoi_file, rotation=Rotation(z=2 * math.pi)),
+            id="Rotate 360 degrees",
+        ),
+        pytest.param(
+            pc_foundation,
+            manipulate_pc(aoi_file, translation=Translation(x=10.0)),
+            id="Translate x=10",
+        ),
+        pytest.param(
+            pc_foundation,
+            manipulate_pc(aoi_file, rotation=Rotation(z=math.pi)),
+            id="Rotate 180 degrees",
+        ),
+        pytest.param(
+            pc_foundation,
+            manipulate_pc(
+                aoi_file,
+                rotation=Rotation(z=math.pi / 2),
+                translation=Translation(x=10_000.0, y=500.0),
+            ),
+            id="Rotate 90 degrees and Translate x=10,000, y=500",
+        ),
+    ],
+)
+def test_registration(foundation: str, aoi: str, tmp_path: pathlib.Path) -> None:
+    output_directory = tmp_path.resolve().as_posix()
+    codem_run_config = codem.CodemRunConfig(
+        foundation, aoi, OUTPUT_DIR=output_directory
+    )
     config = dataclasses.asdict(codem_run_config)
 
-    assert os.path.realpath(tif_fnd) == os.path.realpath(config["FND_FILE"])
-    assert os.path.realpath(tif_aoi) == os.path.realpath(config["AOI_FILE"])
+    assert os.path.realpath(foundation) == os.path.realpath(config["FND_FILE"])
+    assert os.path.realpath(aoi) == os.path.realpath(config["AOI_FILE"])
 
     fnd_obj, aoi_obj = codem.preprocess(config)
 
