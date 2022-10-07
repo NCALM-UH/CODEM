a497500023c294d4b7cb520600f9ca74ac82a42c
Ogi Moore <ognyan.moore@gmail.com>
2022-10-07 11:14:22 -0700

    Add Registration Test Suite for Point-Clouds (#20)
    * Initial go at PC registration testing
    
    * Need glal (not libgdal) to be able to do testing
    
    * use timestamp for output directory only if no output directory specified
    
    * Do registration testing on point clouds with alterations
    
    Add tests where a clipped copy of the foundation file is created, and
    the copy is manipulated via translation and rotation in a number of ways
    and each manipulation is tested against pytest.mark.parametrize
    
    * Expand testing
    
    Add testfile and test the registration of the raster and PC datasets
    through the use of pytest.mark.parametrize
    
    The raster file right now is only cropped per the shapefile, future work
    will involve manipulating the geospatial data within the file.

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,20 +1,82 @@
 import dataclasses
+import itertools
+import math
 import os
-from typing import TYPE_CHECKING
+import pathlib
+import tempfile
 
 import codem
 import pytest
+from point_cloud import manipulate_pc
+from point_cloud import pc_aoi
+from point_cloud import Rotation
+from point_cloud import Translation
+from raster import dem_aoi
 
-if TYPE_CHECKING:
-    from codem.preprocessing.preprocess import GeoData
+
+aoi_shapefile = os.path.abspath("tests/data/aoi_shapefile/aoi.shp")
+dem_foundation = os.path.abspath("tests/data/dem.tif")
+pc_foundation = os.path.abspath("tests/data/pc.laz")
+temporary_directory = os.path.abspath("tests/data/temporary")
 
 
-def test_basic_tif_registration(tif_fnd: GeoData, tif_aoi: GeoData) -> None:
-    codem_run_config = codem.CodemRunConfig(tif_fnd, tif_aoi)
+def make_pc_aoi(aoi_temp_directory: str = temporary_directory) -> str:
+    return pc_aoi(aoi_temp_directory, pc_foundation, aoi_shapefile)
+
+
+def make_raster_aoi(aoi_temp_directory: str = temporary_directory) -> str:
+    return dem_aoi(aoi_temp_directory, dem_foundation, aoi_shapefile)
+
+
+pc_aoi_file = make_pc_aoi()
+raster_aoi_file = make_raster_aoi()
+
+pc_aoi_alterations = [
+    pytest.param(pc_aoi_file, id="PC AOI Original"),
+    pytest.param(
+        manipulate_pc(pc_aoi_file, rotation=Rotation(z=2 * math.pi)),
+        id="PC AOI Rotate 360 degrees",
+    ),
+    pytest.param(
+        manipulate_pc(pc_aoi_file, translation=Translation(x=10.0)),
+        id="PC AOI Translate x=10",
+    ),
+    pytest.param(
+        manipulate_pc(pc_aoi_file, rotation=Rotation(z=math.pi)),
+        id="PC AOI Rotate 180 degrees",
+    ),
+    pytest.param(
+        manipulate_pc(
+            pc_aoi_file,
+            rotation=Rotation(z=math.pi / 2),
+            translation=Translation(x=10_000.0, y=500.0),
+        ),
+        id="PC AOI Rotate 90 degrees and Translate x=10,000, y=500",
+    ),
+]
+
+dem_aoi_alterations = [pytest.param(raster_aoi_file, id="DEM AOI Original")]
+
+
+@pytest.mark.parametrize(
+    "aoi", itertools.chain(pc_aoi_alterations, dem_aoi_alterations)
+)
+@pytest.mark.parametrize(
+    "foundation",
+    [
+        pytest.param(pc_foundation, id="PC Foundation"),
+        pytest.param(dem_foundation, id="DEM Foudnation"),
+    ],
+)
+def test_registration(foundation: str, aoi: str, tmp_path: pathlib.Path) -> None:
+    output_directory = tmp_path.resolve().as_posix()
+    codem_run_config = codem.CodemRunConfig(
+        foundation, aoi, OUTPUT_DIR=output_directory
+    )
     config = dataclasses.asdict(codem_run_config)
 
-    assert os.path.realpath(tif_fnd) == os.path.realpath(config["FND_FILE"])
-    assert os.path.realpath(tif_aoi) == os.path.realpath(config["AOI_FILE"])
+    assert os.path.realpath(foundation) == os.path.realpath(config["FND_FILE"])
+    assert os.path.realpath(aoi) == os.path.realpath(config["AOI_FILE"])
 
     fnd_obj, aoi_obj = codem.preprocess(config)
 
