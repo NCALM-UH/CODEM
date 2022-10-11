42bd3ef12b589671e405910c6f9c6c1bb209b4c1
Ognyan Moore <ognyan.moore@gmail.com>
2022-10-11 15:27:28 -0700

    Migrate to rich, fix logger

--- src/codem/lib/log.py
+++ src/codem/lib/log.py
@@ -6,7 +6,6 @@ Date: February 2021
 A module for setting up logging.
 """
 import logging
-import sys
 import os
 
 
@@ -21,10 +20,13 @@ class Log:
             Verbose mode switch
         """
 
-        self.logger = logging.getLogger()
+        self.logger = logging.getLogger("codem")
         self.logger.setLevel(logging.DEBUG)
+
+        # disable loggers
         logging.getLogger("matplotlib.font_manager").disabled = True
 
+        # configure Formatting
         log_format = logging.Formatter(
             "%(asctime)s - %(name)s - %(levelname)s - %(funcName)s:%(lineno)d - %(message)s"
         )
@@ -43,11 +45,3 @@ class Log:
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(log_format)
         self.logger.addHandler(file_handler)
-
-        console_handler = logging.StreamHandler(sys.stdout)
-        if config["VERBOSE"]:
-            console_handler.setLevel(logging.DEBUG)
-        else:
-            console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(print_format)
-        self.logger.addHandler(console_handler)
