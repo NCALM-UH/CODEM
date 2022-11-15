99e32d7ee7e9c9bb8ecb63b6bdba5bbddb1c7361
Will Forker <will.forker@geoacuity.com>
2022-11-15 14:28:01 -0800

    Added preliminary VCD toolbox code

---
+++ src/vcd/__init__.py
@@ -0,0 +1,5 @@
+from vcd.main import VcdRunConfig
+from vcd.meshing import Mesh
+from vcd.preprocessing import PointCloud
+from vcd.preprocessing import VCD
+from vcd.preprocessing import VCDParameters
